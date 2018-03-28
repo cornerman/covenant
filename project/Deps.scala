c7bfb445b6246a104c8f848287db89c1724e61e6
johannes karoff <johannes@karoff.net>
2018-03-28 22:58:48 +0200

    update dependencies

--- project/Deps.scala
+++ project/Deps.scala
@@ -10,8 +10,8 @@ object Deps {
   val scalajs = new {
     val dom = dep("org.scala-js" %%% "scalajs-dom" % "0.9.5")
   }
-  val sloth = dep("com.github.cornerman.sloth" %%% "sloth" % "4964715")
-  val mycelium = dep("com.github.cornerman.mycelium" %%% "mycelium" % "18ec445")
+  val sloth = dep("com.github.cornerman.sloth" %%% "sloth" % "934d5f6")
+  val mycelium = dep("com.github.cornerman.mycelium" %%% "mycelium" % "be0d69c")
   val kittens = dep("org.typelevel" %%% "kittens" % "1.0.0-RC2")
   val akka = new {
     private val version = "2.5.11"
@@ -19,7 +19,7 @@ object Deps {
     val stream = dep("com.typesafe.akka" %% "akka-stream" % version)
     val actor = dep("com.typesafe.akka" %% "akka-actor" % version)
   }
-  val boopickle = dep("io.suzaku" %%% "boopickle" % "1.2.6")
-  val scribe = dep("com.outr" %%% "scribe" % "2.2.1")
-  val monix = dep("io.monix" %%% "monix" % "3.0.0-M3")
+  val boopickle = dep("io.suzaku" %%% "boopickle" % "1.3.0")
+  val scribe = dep("com.outr" %%% "scribe" % "2.3.0")
+  val monix = dep("io.monix" %%% "monix" % "3.0.0-RC1")
 }
