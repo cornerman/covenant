18733238de8ea3778fdebd53316631c5d4427ad0
johannes karoff <johannes@karoff.net>
2018-04-12 15:58:40 +0200

    update scribe

--- project/Deps.scala
+++ project/Deps.scala
@@ -11,7 +11,7 @@ object Deps {
     val dom = dep("org.scala-js" %%% "scalajs-dom" % "0.9.5")
   }
   val sloth = dep("com.github.cornerman.sloth" %%% "sloth" % "934d5f6")
-  val mycelium = dep("com.github.cornerman.mycelium" %%% "mycelium" % "be0d69c")
+  val mycelium = dep("com.github.cornerman.mycelium" %%% "mycelium" % "90e144f")
   val kittens = dep("org.typelevel" %%% "kittens" % "1.0.0-RC2")
   val akka = new {
     private val version = "2.5.11"
@@ -20,6 +20,6 @@ object Deps {
     val actor = dep("com.typesafe.akka" %% "akka-actor" % version)
   }
   val boopickle = dep("io.suzaku" %%% "boopickle" % "1.3.0")
-  val scribe = dep("com.outr" %%% "scribe" % "2.3.0")
+  val scribe = dep("com.outr" %%% "scribe" % "2.3.2")
   val monix = dep("io.monix" %%% "monix" % "3.0.0-RC1")
 }
