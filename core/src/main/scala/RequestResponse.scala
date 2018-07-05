afe67bee0fff07519520496814fe159db5d7c57e
johannes karoff <johannes@karoff.net>
2018-07-05 11:51:26 +0200

    more

--- core/src/main/scala/RequestResponse.scala
+++ core/src/main/scala/RequestResponse.scala
@@ -12,20 +12,27 @@ sealed trait RequestResponse[+State, +ErrorType, +T] {
   def mapError[E](f: ErrorType => E): RequestResponse[State, E, T]
 }
 object RequestResponse extends RequestType {
-  sealed trait Value[+ErrorType, +T] extends RequestResponse[Nothing, ErrorType, T] {
-    def map[R](f: T => R): Value[ErrorType, R]
-    def mapError[E](f: ErrorType => E): Value[E, T]
+  sealed trait Value[State, +ErrorType, +T] extends RequestResponse[State, ErrorType, T] {
+    def map[R](f: T => R): Value[State, ErrorType, R]
+    def mapError[E](f: ErrorType => E): Value[State, E, T]
   }
-  case class Single[ErrorType, T](task: SingleF[Task, ErrorType, T]) extends Value[ErrorType, T] {
+  sealed trait PureValue[+ErrorType, +T] extends Value[Nothing, ErrorType, T] {
+    def map[R](f: T => R): PureValue[ErrorType, R]
+    def mapError[E](f: ErrorType => E): PureValue[E, T]
+  }
+  case class Single[ErrorType, T](task: SingleF[Task, ErrorType, T]) extends PureValue[ErrorType, T] {
     def map[R](f: T => R): Single[ErrorType, R] = Single(task.map(_.map(f)))
     def mapError[E](f: ErrorType => E): Single[E, T] = Single(task.map(_.left.map(f)))
   }
-  case class Stream[ErrorType, T](task: StreamF[Task, Observable, ErrorType, T]) extends Value[ErrorType, T] {
+  case class Stream[ErrorType, T](task: StreamF[Task, Observable, ErrorType, T]) extends PureValue[ErrorType, T] {
     def map[R](f: T => R): Stream[ErrorType, R] = Stream(task.map(_.map(_.map(f))))
-    def flatMap[R](f: T => R): Stream[ErrorType, R] = Stream(task.map(_.map(_.map(f))))
     def mapError[E](f: ErrorType => E): Stream[E, T] = Stream(task.map(_.left.map(f)))
   }
-  case class StateFunction[State, ErrorType, T](function: State => Value[ErrorType, T]) extends RequestResponse[State, ErrorType, T] {
+  case class StateWithValue[State, ErrorType, T](state: State, value: PureValue[ErrorType, T]) extends Value[State, ErrorType, T] {
+    def map[R](f: T => R): StateWithValue[State, ErrorType, R] = copy(value = value.map(f))
+    def mapError[E](f: ErrorType => E): StateWithValue[State, E, T] = copy(value = value.mapError(f))
+  }
+  case class StateFunction[State, ErrorType, T](function: State => Value[State, ErrorType, T]) extends RequestResponse[State, ErrorType, T] {
     def map[R](f: T => R): StateFunction[State, ErrorType, R] = StateFunction(function andThen (_.map(f)))
     def mapError[E](f: ErrorType => E): StateFunction[State, E, T] = StateFunction(function andThen (_.mapError(f)))
   }
@@ -39,8 +46,6 @@ object RequestResponse extends RequestType {
   implicit def fromTaskEitherObservable[ErrorType, State]: ResultMapping[StreamF[Task, Observable, ErrorType, ?], RequestResponse[State, ErrorType, ?]] = ResultMapping[StreamF[Task, Observable, ErrorType, ?], RequestResponse[State, ErrorType, ?]](Lambda[StreamF[Task, Observable, ErrorType, ?] ~> RequestResponse[State, ErrorType, ?]](Stream(_)))
   implicit def formFutureEitherObservable[ErrorType, State]: ResultMapping[StreamF[Future, Observable, ErrorType, ?], RequestResponse[State, ErrorType, ?]] = ResultMapping[StreamF[Future, Observable, ErrorType, ?], RequestResponse[State, ErrorType, ?]](Lambda[StreamF[Future, Observable, ErrorType, ?] ~> RequestResponse[State, ErrorType, ?]](f => Stream(Task.fromFuture(f))))
 
-  //TODO contramap on resultmapping?
-  //TODO why not Nothing as ErrorType? is covariant
   implicit def fromTask[ErrorType, State]: ResultMapping[Task, RequestResponse[State, ErrorType, ?]] = fromTaskEither.contramapK(Lambda[Task ~> SingleF[Task, ErrorType, ?]](_.map(Right.apply)))
   implicit def fromFuture[ErrorType, State](implicit ec: ExecutionContext): ResultMapping[Future, RequestResponse[State, ErrorType, ?]] = fromFutureEither.contramapK(Lambda[Future ~> SingleF[Future, ErrorType, ?]](_.map(Right.apply)))
   implicit def fromObservable[ErrorType, State]: ResultMapping[Observable, RequestResponse[State, ErrorType, ?]] = fromTaskEitherObservable.contramapK(Lambda[Observable ~> StreamF[Task, Observable, ErrorType, ?]](o => Task.pure(Right(o))))
