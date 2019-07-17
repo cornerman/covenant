d4ac23f6595cafd4f863552ed2ca1d40454b2587
johannes karoff <johannes@karoff.net>
2019-07-17 23:53:19 +0200

    update dependencies

--- core/src/main/scala/DefaultLogHandler.scala
+++ core/src/main/scala/DefaultLogHandler.scala
@@ -8,11 +8,12 @@ import scala.concurrent.{Future, ExecutionContext}
 class DefaultLogHandler[Result[_]](f: Result[_] => Future[Any])(implicit ec: ExecutionContext) extends LogHandler[Result] {
   import LogHelper._
 
-  override def logRequest(path: List[String], arguments: Product, result: Result[_]): Unit = {
+  override def logRequest[T](path: List[String], arguments: Product, result: Result[T]): Result[T] = {
     val watch = StopWatch.started
     scribe.info(s"--> ${requestLogLine(path, arguments)}.")
     f(result).onComplete { result =>
       scribe.info(s"<-- ${requestLogLine(path, arguments, result)}. Took ${watch.readHuman}.")
     }
+    result
   }
 }
--- project/Deps.scala
+++ project/Deps.scala
@@ -10,11 +10,11 @@ object Deps {
   val scalajs = new {
     val dom = dep("org.scala-js" %%% "scalajs-dom" % "0.9.7")
   }
-  val sloth = dep("com.github.cornerman.sloth" %%% "sloth" % "206b005")
-  val mycelium = dep("com.github.cornerman.mycelium" %%% "mycelium" % "44906c6")
-  val kittens = dep("org.typelevel" %%% "kittens" % "1.0.0")
+  val sloth = dep("com.github.cornerman.sloth" %%% "sloth" % "fc487b6")
+  val mycelium = dep("com.github.cornerman.mycelium" %%% "mycelium" % "2117819")
+  val kittens = dep("org.typelevel" %%% "kittens" % "1.2.1")
   val akka = new {
-    private val version = "2.5.11"
+    private val version = "2.5.23"
     val http = dep("com.typesafe.akka" %% "akka-http" % "10.1.9")
     val stream = dep("com.typesafe.akka" %% "akka-stream" % version)
     val actor = dep("com.typesafe.akka" %% "akka-actor" % version)
