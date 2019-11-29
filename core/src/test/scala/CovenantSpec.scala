60082764c114707c130eab071b605eb0603f0c20
Scala Steward <me@scala-steward.org>
2019-11-29 13:12:47 +0100

    Update scalatest to 3.1.0

--- core/src/test/scala/CovenantSpec.scala
+++ core/src/test/scala/CovenantSpec.scala
@@ -1,8 +1,10 @@
 package test.covenant
 
 import org.scalatest._
+import org.scalatest.freespec.AsyncFreeSpec
+import org.scalatest.matchers.must.Matchers
 
-class CovenantSpec extends AsyncFreeSpec with MustMatchers {
+class CovenantSpec extends AsyncFreeSpec with Matchers {
   "true" in {
     true mustEqual true
   }
